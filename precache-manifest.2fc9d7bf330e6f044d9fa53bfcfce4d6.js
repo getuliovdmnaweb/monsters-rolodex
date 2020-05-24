ac8af2cb052bca5c209ba67072154e7210d76f5f
Getulio <getuliovdmnaweb@gmail.com>
2020-05-24 00:14:48 -0300

    Updates

---
+++ precache-manifest.2fc9d7bf330e6f044d9fa53bfcfce4d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17144a45022f4a1d283965eb3e485372",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1789e2267487150ce6a7",
+    "url": "/monsters-rolodex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8f2b4c631cd17f4cbe49",
+    "url": "/monsters-rolodex/static/js/2.aa517fed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/monsters-rolodex/static/js/2.aa517fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1789e2267487150ce6a7",
+    "url": "/monsters-rolodex/static/js/main.72da2b9e.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
